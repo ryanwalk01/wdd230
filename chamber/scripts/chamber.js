2947089646ed5cc5fe41f6d6169530fad63692c1
ryanwalk01 <ryan.walk01@gmail.com>
2024-04-26 14:47:27 -0600

    Added Chambers home page

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,12 @@
+// Last modified
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+// Hamburger Menu
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.nav');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
