96585b3b3400710616dce9cca1e8b5965d595950
ryanwalk01 <ryan.walk01@gmail.com>
2024-04-30 11:43:27 -0600

    Added W03 Learning Activities

---
+++ week03/scripts/bom.js
@@ -0,0 +1,51 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value == '') {
+        input.focus();
+
+    }
+    else {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value = '';
+        input.focus();
+    }
+})
+
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+function displayList(item) {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    li.append(deleteButton);
+    list.append(li);
+    deleteButton.addEventListener('click', () => {
+        list.removeChild(li);
+        deleteChapter(li.textContent);
+        input.focus();
+    });
+}
+
+function setChapterList(chaptersArray) {
+    localStorage.setItem('#list'), JSON.stringify(chaptersArray); 
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('#list'));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter((item) => item !== chapter);
+    setChapterList();
+}
