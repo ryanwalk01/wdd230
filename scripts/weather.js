1cc5fe6b476e7d68dc5c4a9c8b83b143edb7c1cb
ryanwalk01 <ryan.walk01@gmail.com>
2024-05-23 13:18:58 -0600

    W05 Team update

---
+++ scripts/weather.js
@@ -0,0 +1,33 @@
+const currentTemp = document.querySelector('#current-temp');
+const description = document.querySelector('#description');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=39.26&lon=-81.56&units=imperial&appid=87735150c2dde6d1d1dd1ff5ebc988f4';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F -`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    description.innerHTML = desc;
+}
+
+apiFetch();
